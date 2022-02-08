2b504657225c016db9f8cd6a82c850977e5edcaa
Garrett Carr <gcarr@byu.edu>
2022-02-08 10:15:58 -0700

    added changes

--- linreg.R
+++ linreg.R
@@ -42,4 +42,29 @@ geweke.diag(sims)
 chains[1,]
 b0 <- chains[,1]
 b1 <- chains[,2]
-plot(density(b1))
+vr <- chains[,4]
+plot(x,y)
+abline(b0, b1, add = T)
+
+
+### Inference
+
+# Post probability
+xnew <- seq(7, 13, by = 0.5)
+quantile(b0 + 7 * b1, c(0.025, 0.5, 0.975))
+test <- sapply(xnew, function(x) x*b1 + b0)
+quantile(test[,1], c(0.025, 0.5, 0.975))
+
+ul <- apply(test, 2, quantile, 0.975)
+ll <- apply(test, 2, quantile, 0.025)
+
+# Posterior probability around the line
+lines(xnew, ul, col = 'red')
+lines(xnew, ll, col = 'red')
+
+testpred <- test + rnorm(12000, 0, sqrt(vr))
+upi <- apply(testpred, 2, quantile, 0.975)
+lpi <- apply(testpred, 2, quantile, 0.025)
+
+lines(xnew, upi, col = 'blue')
+lines(xnew, lpi, col = 'blue')
